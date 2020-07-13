239f7eb4d72f4c2f6f5358eb1bd587f67c5ebcbc
Riyufuchi <56110060+Riyufuchi@users.noreply.github.com>
2020-07-13 19:47:42 +0200

    Add files via upload

---
+++ Console/MainSource.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+/*
+* Copyright Header
+*
+* Created On: 13.07.2020
+* Last Edit: 13.07.2020
+* Created By: Riyufuchi
+*
+*/
+
+int main(int argc, char **argv)
+{
+	cout << "Hello world, it's living time." << endl;
+	cin.get();
+	//return 0;
+}
